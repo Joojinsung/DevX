1b51270599ae86e81d350f446e09d0b5645d4bf5
Joojinsung <dev.jinsung1017@gmail.com>
2024-04-30 14:35:50 +0900

    :tada:1-초기세팅

---
+++ src/test/java/com/backend/devx/DevXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.devx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
